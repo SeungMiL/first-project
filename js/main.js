e57ee1129911b1942e5a1ddc0fb75dfa64c6f171
SeungMin Lee <tmskqj@naver.com>
2021-11-29 22:53:03 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,76 @@
+
+// 끝말잇기&쿵쿵따 기본변수
+
+const number = Number(prompt('몇 명이 참가하나요?'));
+const $button = document.querySelector('button');
+const $input = document.querySelector('input');
+const $word = document.querySelector('#word')
+const $order = document.querySelector('#order');
+let word;
+let newWord;
+
+
+
+
+// 쿵쿵따
+
+const onClickButton = () => {
+    if(newWord.length === 3 && (!word || word[word.length - 1] === newWord[0])){
+        word = newWord;
+        $word.textContent = word;
+        const order = Number($order.textContent);
+        if(order + 1 > number){
+            $order.textContent = 1;
+        } else {
+            $order.textContent = order + 1;
+        }
+    } else {
+        alert('올바르지 않은 단어입니다!');
+    }
+    $input.value = '';
+    $input.focus();
+};
+
+const onInput = (event) => {
+    newWord = event.target.value;
+}
+
+if(number){
+    const $button = document.querySelector('button');
+    const $input = document.querySelector('input');
+    $button.addEventListener('click', onClickButton);
+    $input.addEventListener('input', onInput);
+} 
+
+
+
+
+
+// 끝말잇기
+
+// const onclickButton = () => {
+//     if(!word || word[word.length -1]===newWord[0]){
+//         word = newWord;
+//         $word.textContent = word;
+//         const order = Number($order.textContent);
+//         if(order + 1 > number){
+//             $order.textContent = 1;
+//         } else {
+//             $order.textContent = order +1;
+//         }
+//     }  else {
+//             alert('올바르지 않은 단어입니다!')
+
+//         }
+//         $input.value = '';
+//         $input.focus();
+//     };
+
+// const onInput = (event) => {
+//     newWord = event.target.value;
+// }
+
+// $button.addEventListener('click', onclickButton);
+// $input.addEventListener('input', onInput);
+
+
